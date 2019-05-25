791c9aeb89a96e8e9b63eb7327d9c8e0ec9e0658
Alejandro Mancilla <alejandro.mancilla.umsa@gmail.com>
2019-05-25 02:53:47 -0400

    implementamos el boton de logearse con usuario y contraseña

--- app/src/main/java/com/example/registrodeusuariosconfirebases/MainActivity.java
+++ app/src/main/java/com/example/registrodeusuariosconfirebases/MainActivity.java
@@ -28,7 +28,8 @@ public class MainActivity extends AppCompatActivity {
 
     private EditText editText_user;
     private EditText editText_pass;
-    Button button_registrar;
+    private Button button_registrar;
+    private Button button_login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,6 +39,7 @@ public class MainActivity extends AppCompatActivity {
         editText_user = findViewById(R.id.editText_Email);
         editText_pass = findViewById(R.id.editText_contrasenia);
         button_registrar = findViewById(R.id.button_registro);
+        button_login = findViewById(R.id.button_login);
 
         firebaseAuth = FirebaseAuth.getInstance();
 
@@ -96,6 +98,55 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    public void login (View v)
+    {
+        // el trim es para eliminar espacios que tengamos al principio y al final
+        String email = editText_user.getText().toString().trim();
+        String pass = editText_pass.getText().toString().trim();
+
+        //verificamos que las cajas de texto no esten vacias
+        if(TextUtils.isEmpty(email))
+        {
+            Toast.makeText(this, "Debe colocar un Email en la caja de Email.", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if(TextUtils.isEmpty(pass))
+        {
+            Toast.makeText(this, "Debe colocar una Contraseña en la caja de contraseña.", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        //en caso de que se lleno las cajas de usuario y contrasenia
+        //se mostrara la barra de progreso
+        progresDialog.setMessage("Realizando registro en linea...");
+        progresDialog.show();
+
+        //consultar si un usuario existe
+        firebaseAuth.signInWithEmailAndPassword(email,pass)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful())
+                        {
+                            Toast.makeText(MainActivity.this, "Bienvenido", Toast.LENGTH_SHORT).show();
+                        }
+                        else
+                        {
+                            if(task.getException() instanceof FirebaseAuthUserCollisionException)//si se presenta una colicion
+                            {
+                                Toast.makeText(MainActivity.this, "El usuario que intenta registrar ya se encuentra registrado", Toast.LENGTH_SHORT).show();
+                            }
+                            else
+                            {
+                                Toast.makeText(MainActivity.this, "No se pudo registrar el usuario, intentelo nuevamente", Toast.LENGTH_SHORT).show();
+                            }
+
+                        }
+                        progresDialog.dismiss();
+                    }
+                });
+
+    }
     @Override
     protected void onStart() {
         super.onStart();
