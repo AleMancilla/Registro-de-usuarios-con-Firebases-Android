ddc8719e3945db6bad2e7bcc79876772c41c5dd2
Alejandro Mancilla <alejandro.mancilla.umsa@gmail.com>
2019-05-26 13:45:41 -0400

    agregamos leer datos de usuario en 3 editText nombre, email,Id

--- app/src/main/java/com/example/registrodeusuariosconfirebases/SegundaPantalla.java
+++ app/src/main/java/com/example/registrodeusuariosconfirebases/SegundaPantalla.java
@@ -5,13 +5,18 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class SegundaPantalla extends AppCompatActivity {
 
     private Button button_cerrar_sesion;
-    FirebaseAuth firebaseAuth;
+    private FirebaseAuth firebaseAuth;
+    private FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
+    private TextView textView_name, textView_email, textView_urlPhoto, textView_emailVerification, textView_uId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +25,32 @@ public class SegundaPantalla extends AppCompatActivity {
 
         button_cerrar_sesion = findViewById(R.id.button_cerrar_sesion);
         firebaseAuth = FirebaseAuth.getInstance();
+
+        textView_email = findViewById(R.id.textView_email);
+        textView_name = findViewById(R.id.textView_username);
+        textView_emailVerification = findViewById(R.id.textView_emailVerification);
+        textView_urlPhoto = findViewById(R.id.textView_urlphoto);
+        textView_uId = findViewById(R.id.textView_IDUSER);
+
+        if(user != null)
+        {
+            String name = user.getDisplayName();
+            String email = user.getEmail();
+            //String urlPhoto = user.getPhotoUrl().toString();
+
+            //boolean emailVeridied = user.isEmailVerified();
+            String idUser = user.getUid();
+
+            //String emailVer=emailVeridied +" ";
+
+            textView_name.setText(name);
+            textView_email.setText(email);
+            //textView_emailVerification.setText(emailVer);
+            //textView_urlPhoto.setText(urlPhoto);
+            textView_uId.setText(idUser);
+
+        }
+
     }
 
     public void cerrarSesion(View v)
