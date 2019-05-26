323a650b13b4588b10c5373c5ea7862a9499bd55
Alejandro Mancilla <alejandro.mancilla.umsa@gmail.com>
2019-05-26 18:34:31 -0400

    realizamos una consulta a la base de datos sobre los datos de un usuario buscando su correo electronico, solo funciona para correos existentes

--- app/src/main/java/com/example/registrodeusuariosconfirebases/SegundaPantalla.java
+++ app/src/main/java/com/example/registrodeusuariosconfirebases/SegundaPantalla.java
@@ -1,14 +1,24 @@
 package com.example.registrodeusuariosconfirebases;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 public class SegundaPantalla extends AppCompatActivity {
 
@@ -16,8 +26,9 @@ public class SegundaPantalla extends AppCompatActivity {
     private FirebaseAuth firebaseAuth;
     private FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
 
-    private TextView textView_name, textView_email, textView_urlPhoto, textView_emailVerification, textView_uId;
+    private TextView textView_name,textView_Lastname, textView_email, textView_urlPhoto, textView_emailVerification, textView_uId;
 
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
 
 
     @Override
@@ -30,26 +41,53 @@ public class SegundaPantalla extends AppCompatActivity {
 
         textView_email = findViewById(R.id.textView_email);
         textView_name = findViewById(R.id.textView_username);
+        textView_Lastname=findViewById(R.id.textView_lastname);
         textView_emailVerification = findViewById(R.id.textView_emailVerification);
         textView_urlPhoto = findViewById(R.id.textView_urlphoto);
         textView_uId = findViewById(R.id.textView_IDUSER);
 
         if(user != null)
         {
-            String name = user.getDisplayName();
-            String email = user.getEmail();
-            //String urlPhoto = user.getPhotoUrl().toString();
+            //String name = user.getDisplayName();
+            final String email = user.getEmail();
+            //String idUser = user.getUid();
 
-            //boolean emailVeridied = user.isEmailVerified();
-            String idUser = user.getUid();
 
-            //String emailVer=emailVeridied +" ";
 
-            textView_name.setText(name);
-            textView_email.setText(email);
-            //textView_emailVerification.setText(emailVer);
-            //textView_urlPhoto.setText(urlPhoto);
-            textView_uId.setText(idUser);
+            //leendo la base de datos
+
+
+            db.collection("Usuarios").whereEqualTo("Email", email)
+            .get()
+                    .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                        @Override
+                        public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                            if(task.isSuccessful())
+                            {
+                                for (QueryDocumentSnapshot document : task.getResult()) {
+                                    if(document.getData().get("Email").toString().equals(user.getEmail()))
+                                    {
+                                        textView_name.setText(document.getData().get("Nombre").toString());
+                                        textView_email.setText(document.getData().get("Email").toString());
+                                        textView_uId.setText(document.getData().get("IDUser").toString());
+                                        textView_Lastname.setText(document.getData().get("Apellidos").toString());
+
+                                    }
+                                    Log.d("Aleman", document.getId() + " => " + document.getData());
+                                }
+
+                                Toast.makeText(getApplication(), "EL EMAIL BUSCADO ES "+email, Toast.LENGTH_SHORT).show();
+
+                            }
+                            else
+                            {
+                                //error al adquirir los documentos
+                            }
+                        }
+                    });
+            // termino de leer la base de datos
+
+
 
 
 
